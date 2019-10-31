76553a4217fd2695f856f6ceb610cd2884a13508
Edward Tomasz Napierala <trasz@FreeBSD.org>
2019-10-31 04:37:37 +0000

    Make overhead.py work.

--- overhead.py
+++ overhead.py
@@ -3,16 +3,38 @@
 #
 # Turns a trace, or the output from "trace2cmt -s", eg:
 #
-# malloc(8) = 81d220b48<6655>
-# realloc(81d220b48<6655>, 16) = 81d3b83a0<6656>
-# free(81d3b83a0<6656>)
+# malloc(17) = 81d3bd6c0<6385>
+# malloc(7) = 81d220b40<6386>
+# malloc(18) = 81d3bd6e0<6387>
+# malloc(12) = 81d3b8590<6388>
+# malloc(27) = 81d3bd700<6389>
+# malloc(256) = 81d360600<6390>
+# realloc(81d360600<6390>, 512) = 81d3b9800<6391>
+# realloc(81d3b9800<6391>, 1024) = 81d3bbc00<6392>
+# free(81d220b28<6306>)
+# free(81d3b83b0<6307>)
+# free(81d3b83c0<6308>)
+# free(81d3b83d0<6309>)
+# free(81d3bd100<6310>)
+# free(81d3bd120<6311>)
 #
 # ... into this:
 #
-# malloc(8) = 81d220b48<6655>
-# realloc(81d220b48<6655>, 16) = 81d3b83a0<6656>    # 8 bytes, allocated 1 lines ago
-# free(81d3b83a0<6656>)                             # 16 bytes, allocated 1 lines ago
-#
+# malloc(17) = 81d3bd6c0<6385>                      # prev 81d3bd6a0, gap 12, next 81de0d000, gap 10811695
+# malloc(7) = 81d220b40<6386>                       # prev 81d220b38, gap 0, next 81d221700, gap 3001
+# malloc(18) = 81d3bd6e0<6387>                      # prev 81d3bd6c0, gap 15, next 81de0d000, gap 10811662
+# malloc(12) = 81d3b8590<6388>                      # prev 81d3b8580, gap 0, next 81d3ba200, gap 7268
+# malloc(27) = 81d3bd700<6389>                      # prev 81d3bd6e0, gap 14, next 81de0d000, gap 10811621
+# malloc(256) = 81d360600<6390>                     # prev 81d360300, gap 512, next 81d363800, gap 12544
+# realloc(81d360600<6390>, 512) = 81d3b9800<6391>   # 256 bytes, allocated 1 lines ago, prev 81d360300, gap 512, next 81d363800, gap 12544
+# realloc(81d3b9800<6391>, 1024) = 81d3bbc00<6392>  # 512 bytes, allocated 1 lines ago, prev 81d3b8590, gap 4708, next 81d3ba200, gap 2048
+# free(81d220b28<6306>)                             # 6 bytes, allocated 87 lines ago, prev 81d220b20, gap 1, next 81d220b30, gap 2
+# free(81d3b83b0<6307>)                             # 9 bytes, allocated 87 lines ago, prev 81d3b83a0, gap 4, next 81d3b83c0, gap 7
+# free(81d3b83c0<6308>)                             # 10 bytes, allocated 87 lines ago, prev 81d3b83a0, gap 20, next 81d3b83d0, gap 6
+# free(81d3b83d0<6309>)                             # 16 bytes, allocated 87 lines ago, prev 81d3b83a0, gap 36, next 81d3b83e0, gap 0
+# free(81d3bd100<6310>)                             # 18 bytes, allocated 87 lines ago, prev 81d3bd0e0, gap 0, next 81d3bd120, gap 14
+# free(81d3bd120<6311>)                             # 17 bytes, allocated 87 lines ago, prev 81d3bd0e0, gap 32, next 81d3bd140, gap 15
+# 
 
 import re
 import sys
@@ -31,17 +53,16 @@ def neighbours(ptr):
 	index = sizes.index(ptr)
 	size = sizes[ptr]
 
-	try:
-		prev_pointer, prev_size = sizes.peekitem(index - 1)
-		#print("prev {:x}, prev_size {}, end {:x}, ptr {:x}, gap {}".format(prev_pointer, prev_size, prev_pointer + prev_size, ptr, ptr - prev_pointer + prev_size))
-		#assert prev_pointer < ptr
-		if prev_pointer >= ptr:
-			# wtf
-			prev_pointer = 0
-			prev_size = 0
-		else:
+	if index > 0:
+		try:
+			prev_pointer, prev_size = sizes.peekitem(index - 1)
+			#print("prev {:x}, prev_size {}, end {:x}, ptr {:x}, gap {}".format(prev_pointer, prev_size, prev_pointer + prev_size, ptr, ptr - prev_pointer + prev_size))
+			assert prev_pointer < ptr
 			assert prev_pointer + prev_size <= ptr
-	except IndexError:
+		except IndexError:
+			prev_pointer = None
+			prev_size = None
+	else:
 		prev_pointer = None
 		prev_size = None
 
@@ -64,13 +85,13 @@ def do_free(ptr, lineno):
 	prev_pointer, prev_size, next_pointer, next_size = neighbours(ptr)
 
 	if prev_pointer:
-		prev_gap = ptr - prev_pointer + prev_size
+		prev_gap = ptr - prev_pointer - prev_size
 	else:
 		prev_pointer = 0
 		prev_gap = -1
 
 	if next_pointer:
-		next_gap = next_pointer - ptr + size
+		next_gap = next_pointer - ptr - size
 	else:
 		next_pointer = 0
 		next_gap = -1
@@ -80,18 +101,35 @@ def do_free(ptr, lineno):
 
 	return "{} bytes, allocated {} lines ago, prev {:x}, gap {}, next {:x}, gap {}".format(size, age, prev_pointer, prev_gap, next_pointer, next_gap)
 
+def do_malloc(ptr, size, lineno):
+	assert ptr not in sizes
+	assert ptr not in allocated
+	sizes[ptr] = size
+	allocated[ptr] = lineno
+
+	prev_pointer, prev_size, next_pointer, next_size = neighbours(ptr)
+
+	if prev_pointer:
+		prev_gap = ptr - prev_pointer - prev_size
+	else:
+		prev_pointer = 0
+		prev_gap = -1
+
+	if next_pointer:
+		next_gap = next_pointer - ptr - size
+	else:
+		next_pointer = 0
+		next_gap = -1
+
+	return "prev {:x}, gap {}, next {:x}, gap {}".format(prev_pointer, prev_gap, next_pointer, next_gap)
+
 def do_line(l, lineno):
 	f = re.split("[()=, ]+", l)
 	if f[0] == "malloc":
 		size = int(f[1])
 		ptr = parse_ptr(f[2])
 
-		assert ptr not in sizes
-		assert ptr not in allocated
-		sizes[ptr] = size
-		allocated[ptr] = lineno
-
-		return None
+		return do_malloc(ptr, size, lineno)
 
 	if f[0] == "free":
 		oldptr = parse_ptr(f[1])
@@ -100,15 +138,11 @@ def do_line(l, lineno):
 
 	if f[0] == "realloc":
 		oldptr = parse_ptr(f[1])
-		size = int(f[2])
+		newsize = int(f[2])
 		newptr = parse_ptr(f[3])
 
 		tmp = do_free(oldptr, lineno)
-
-		assert newptr not in sizes
-		assert newptr not in allocated
-		sizes[newptr] = size
-		allocated[newptr] = lineno
+		do_malloc(newptr, newsize, lineno)
 
 		return tmp
 
