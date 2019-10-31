0420dd8d3653a55dab1be7d84931cb30105330a5
Edward Tomasz Napierala <trasz@FreeBSD.org>
2019-10-31 03:58:32 +0000

    WIP

--- overhead.py
+++ overhead.py
@@ -27,33 +27,67 @@ sizes = SortedDict()
 def parse_ptr(l):
 	return int(re.split("[<>]+", l)[0], 16)
 
+def neighbours(ptr):
+	index = sizes.index(ptr)
+	size = sizes[ptr]
+
+	try:
+		prev_pointer, prev_size = sizes.peekitem(index - 1)
+		#print("prev {:x}, prev_size {}, end {:x}, ptr {:x}, gap {}".format(prev_pointer, prev_size, prev_pointer + prev_size, ptr, ptr - prev_pointer + prev_size))
+		#assert prev_pointer < ptr
+		if prev_pointer >= ptr:
+			# wtf
+			prev_pointer = 0
+			prev_size = 0
+		else:
+			assert prev_pointer + prev_size <= ptr
+	except IndexError:
+		prev_pointer = None
+		prev_size = None
+
+	try:
+		next_pointer, next_size = sizes.peekitem(index + 1)
+		assert next_pointer > ptr
+		assert ptr + size <= next_pointer
+	except IndexError:
+		next_pointer = None
+		next_size = None
+
+	return prev_pointer, prev_size, next_pointer, next_size
+
 def do_free(ptr, lineno):
 	size = sizes[ptr]
 	age = lineno - allocated[ptr]
 
 	index = sizes.index(ptr)
 
-	try:
-		prev_pointer, _ = sizes.peekitem(index - 1)
-	except IndexError:
-		prev_pointer = 0
+	prev_pointer, prev_size, next_pointer, next_size = neighbours(ptr)
 
-	try:
-		next_pointer, _ = sizes.peekitem(index + 1)
-	except IndexError:
+	if prev_pointer:
+		prev_gap = ptr - prev_pointer + prev_size
+	else:
+		prev_pointer = 0
+		prev_gap = -1
+
+	if next_pointer:
+		next_gap = next_pointer - ptr + size
+	else:
 		next_pointer = 0
+		next_gap = -1
 
 	del allocated[ptr]
 	del sizes[ptr]
 
-	return "{} bytes, allocated {} lines ago, prev {:x}, next {:x}".format(size, age, prev_pointer, next_pointer)
+	return "{} bytes, allocated {} lines ago, prev {:x}, gap {}, next {:x}, gap {}".format(size, age, prev_pointer, prev_gap, next_pointer, next_gap)
 
 def do_line(l, lineno):
 	f = re.split("[()=, ]+", l)
 	if f[0] == "malloc":
-		size = f[1]
+		size = int(f[1])
 		ptr = parse_ptr(f[2])
 
+		assert ptr not in sizes
+		assert ptr not in allocated
 		sizes[ptr] = size
 		allocated[ptr] = lineno
 
@@ -66,11 +100,13 @@ def do_line(l, lineno):
 
 	if f[0] == "realloc":
 		oldptr = parse_ptr(f[1])
-		size = f[2]
+		size = int(f[2])
 		newptr = parse_ptr(f[3])
 
 		tmp = do_free(oldptr, lineno)
 
+		assert newptr not in sizes
+		assert newptr not in allocated
 		sizes[newptr] = size
 		allocated[newptr] = lineno
 
